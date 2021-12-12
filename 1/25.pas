dbe324d5ef6a436c87135f5de33d75321359868f
levisan857 <alina.rizvanova.2006@gmail.com>
2021-12-12 09:37:36 +0300

    test 1/25

--- 1/25.pas
+++ 1/25.pas
@@ -1,1 +1,8 @@
-// test
+program s1;
+var n:integer;
+begin
+writeln ('введите число');
+readln(n);
+if n mod 3=0 then writeln('кратно трем') else writeln ('не кратно трем');
+if n>0 then writeln ('положительное') else writeln('не положительное');
+end.
